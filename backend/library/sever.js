132db2ad75301947fbb8fab96d68b6b5f4c4e40a
Cheng In Du <cdu@bayviewglen.ca>
2024-11-19 15:04:06 -0500

    server

---
+++ backend/library/sever.js
@@ -0,0 +1,60 @@
+import express, { urlencoded } from 'express';
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+let library = [
+    { id: 1, title: 'To Kill a Mockingbird', author: 'Harper Lee', year: 1960, isAvailable: true },
+    { id: 2, title: '1984', author: 'George Orwell', year: 1949, isAvailable: true },
+    { id: 3, title: 'Moby Dick', author: 'Herman Melville', year: 1851, isAvailable: true }
+];
+
+
+
+app.get('/books/', (req, res) => 
+    {res.send((library))}
+);
+
+
+// add book
+app.post('/books', (req, res) => {
+    const book = {
+        id : library.length + 1,
+        title: req.body.title,
+        author: req.body.author,
+        year: req.body.year,
+        isAvailable: true
+    };
+    library.push(book);
+    res.send(book);
+});
+
+app.get('/books/available', (req, res) => {
+    const availableBooks = library.filter((a) => a.isAvailable);
+    res.send(availableBooks);
+});
+
+app.post('/books/borrow', (req,res) => {
+    const book = library.find(book => book.title === req.body.title && book.isAvailable);
+    if (!book)
+        return res.status(400).send({error : "Book is not available or is already borrowed."});
+    book.isAvailable = false;
+    res.send(book);
+});
+
+app.post('/books/return', (req,res) => {
+    const book = library.find(book => book.title === req.body.title && !book.isAvailable);
+    if (!book)
+        return res.status(400).send({error : "Book is non-existent or is not borrowed."});
+    book.isAvailable = true;
+    res.send(book);
+});
+
+
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
