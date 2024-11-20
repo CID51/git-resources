a2b6afeb9f2ec526eac04a83a9ff392f87cb7f0d
Cheng In Du <cdu@bayviewglen.ca>
2024-11-19 19:15:01 -0500

    fix pls

--- backend/library/sever.js
+++ backend/library/sever.js
@@ -2,7 +2,6 @@ import express, { urlencoded } from 'express';
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
