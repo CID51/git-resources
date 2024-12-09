016dffa608a14841f9496f92b494951e932e1960
Cheng In Du <cdu@bayviewglen.ca>
2024-12-09 13:00:42 -0500

    help me mongods

---
+++ backend/sport-api/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import { connectDB } from './config/db.js';
+import aggregationRoutes from './routes/aggregation.js';
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+
+
+app.use(express.json());
+
+app.use('/api/aggregation', aggregationRoutes);
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
